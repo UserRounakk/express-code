fcb3cdd745a6fdfecb405f53897134b4fbc8201c
Rounak Agrawal <rounak1430@gmail.com>
2022-07-25 19:39:44 +0545

    Added auth basics

--- authgen.js
+++ authgen.js
@@ -5,7 +5,138 @@ const inflect = require('i')();
 
 function authgen() {
     if (process.argv[2] == "auth") {
-        console.log('test successful');
+        //create app.js file
+        if (!fs.existsSync('../app.js')) {
+            try {
+                fs.writeFile('../app.js', content.app, function (err) {
+                    if (err) throw err;
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        // Create app folder if it doesnt exist
+        if (!fs.existsSync('../app')) {
+            fs.mkdirSync("../app")
+        }
+
+        //create config folder
+        if (!fs.existsSync('../config')) {
+            fs.mkdirSync("../config")
+        }
+
+        //creating config file with definations
+        if (!fs.existsSync('../config/config.js')) {
+            try {
+                fs.writeFile('../config/config.js', content.config, function (err) {
+                    if (err) throw err;
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        //create routes folder
+        if (!fs.existsSync('../routes')) {
+            fs.mkdirSync("../routes")
+        }
+        //creating routes.js
+        if (!fs.existsSync('../routes/routes.js')) {
+            try {
+                fs.writeFileSync('../routes/routes.js', content.route, { encoding: "utf-8", flag: "w" });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        //creating auth routes content
+        if (!fs.existsSync(`../routes/auth.route.js`)) {
+            try {
+                fs.writeFileSync(`../routes/auth.routes.js`, content.authRoute, { encoding: "utf-8", flag: "w" });
+
+                //import new route into main route
+                appendPos('../routes/routes.js', 59, content.newRoute.require('auth'));
+
+                //use new route in the main route
+                appendEndPos('../routes/routes.js', 23, content.newRoute.authUse);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        //creating user routes content
+        if (!fs.existsSync(`../routes/user.route.js`)) {
+            try {
+                fs.writeFileSync(`../routes/user.routes.js`, content.newRoute.content('user'), { encoding: "utf-8", flag: "w" });
+
+                //import new route into main route
+                appendPos('../routes/routes.js', 59, content.newRoute.require('user'));
+
+                //use new route in the main route
+                appendEndPos('../routes/routes.js', 23, content.newRoute.use('user'));
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        //creating user model
+        if (!fs.existsSync('../app/models')) {
+            fs.mkdirSync("../app/models", { recursive: true })
+        }
+        if (!fs.existsSync(`../app/models/user.model.js`)) {
+            try {
+                fs.writeFileSync(`../app/models/user.model.js`, content.userModel, { encoding: "utf-8", flag: "w" });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        //creating controllers
+        if (!fs.existsSync('../app/controllers')) {
+            fs.mkdirSync("../app/controllers", { recursive: true })
+        }
+        //auth controller
+        if (!fs.existsSync(`../app/controllers/auth.controller.js`)) {
+            try {
+                fs.writeFileSync(`../app/controllers/auth.controller.js`, content.authController, { encoding: "utf-8", flag: "w" });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        //creating middlewares
+        if (!fs.existsSync('../app/middlewares')) {
+            fs.mkdirSync("../app/middlewares", { recursive: true })
+        }
+        //uploader middleware
+        if (!fs.existsSync(`../app/middlewares/uploader.middleware.js`)) {
+            try {
+                fs.writeFileSync(`../app/middlewares/uploader.middleware.js`, content.uploader, { encoding: "utf-8", flag: "w" });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        //auth middleware
+        if (!fs.existsSync(`../app/middlewares/auth.middleware.js`)) {
+            try {
+                fs.writeFileSync(`../app/middlewares/auth.middleware.js`, content.authMiddleware, { encoding: "utf-8", flag: "w" });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        //creating service
+        if (!fs.existsSync('../app/services')) {
+            fs.mkdirSync("../app/services", { recursive: true })
+        }
+        if (!fs.existsSync(`../app/services/auth.service.js`)) {
+            try {
+                fs.writeFileSync(`../app/services/auth.service.js`, content.authService, { encoding: "utf-8", flag: "w" });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
     }
 }
 
--- content.js
+++ content.js
@@ -3,7 +3,8 @@ const content = {
     config:
         `const CONFIG = {
     DB_URL: 'db_name',
-    DB_NAME: 'db_url'
+    DB_NAME: 'db_url',
+    JWT_SECRET: "jwt_secret"
 };
 module.exports = CONFIG;`,
 
@@ -101,6 +102,7 @@ module.exports = app;
         use: (name) => {
             return `\napp.use('/${inflect.pluralize(name)}', ${name}_routes);`
         },
+        authUse: `\napp.use(auth_routes);`,
         content: (name) => {
             let snake_name = inflect.pluralize(name) + '_controller'
             let controller = inflect.camelize(snake_name);
@@ -168,7 +170,306 @@ class ${name}Service {
 }
 
 module.exports = ${name}Service;`
+    },
+
+    userModel: `const mongoose = require('mongoose');
+const AddressSchemaDef = new mongoose.Schema({
+    name: String,
+    street_name: String,
+    house_no: Number
+})
+
+const UserSchemaDef = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email:  {
+        type: String,
+        required: [true, "Email is required"],
+        validate: {
+            validator: function(em){
+                // text@text.text
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(em)
+            },
+            message: props => "Invalid email."
+        },
+        unique: [true, "Email should be unique"]
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    role: [{
+        type: String,
+        enum: ["admin", 'seller','customer'],
+        default: "customer"
+    }] ,
+    status: {
+        type: String,
+        enum: ["active", 'inactive'],
+        default: "active"
+    } ,
+    image: {
+        type: String,
+        default: null
+    },
+    phone: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: props => "Invalid phone number!"
+        },
+    },
+    address: {
+        billing: AddressSchemaDef,
+        shipping: AddressSchemaDef
     }
+}, {
+    timestamps: true,
+    autoIndex: true,
+    autoCreate: true
+});
+
+// User => users
+const UserModel = mongoose.model('User', UserSchemaDef);
+module.exports = UserModel;`,
+
+    authController: `const AuthService = require("../services/auth.service");
+const bcrypt = require("bcrypt");
+
+class AuthController {
+
+    constructor() {
+        this.auth_service = new AuthService();
+    }
+
+    setPath = (req,res,next)=>{
+        req.dir = "public/uploads/user";
+        next();
+    }
+
+    login = async (req, res, next) => {
+        try {
+            let data = req.body;
+            let validate = this.auth_service.loginValidate(data);
+
+            if (validate) {
+                next({
+                    status: 400,
+                    msg: validate
+                });
+            } else {
+                let user = await this.auth_service.login(data);
+                let token = this.auth_service.getToken({
+                    id: user._id,
+                    name: user.name
+                });
+                res.json({
+                    result: {
+                        user: user.name + "has been logged in successfully.",
+                        access_token: token
+                    },
+                    status: true,
+                    msg: "User logged in successfully."
+                })
+            }
+        } catch (err) {
+            next({
+                status: 500,
+                msg: err
+            });
+        }
+    }
+
+    register = async (req, res, next) => {
+        try {
+            let data = req.body;
+
+            if (req.file) {
+                data.image = req.file.filename;
+            }
+            this.auth_service.registerValidate(data);
+
+            data.password = bcrypt.hashSync(data.password, 10);
+
+            this.auth_service.register(data)
+                .then((succ) => {
+                    res.json({
+                        status: true,
+                        msg: "User registered",
+                        result: data
+                    })
+                }).catch((err) => {
+                    next({
+                        status: 400,
+                        msg: err
+                    })
+                })
+
+        } catch (error) {
+            next({
+                status: 422,
+                msg: error
+            })
+        }
+    }
+}
+
+module.exports = AuthController;
+`,
+    authMiddleware:
+        `const jwt = require("jsonwebtoken");
+const CONFIG = require("../../config/config");
+const UserModel = require("../models/user.model");
+
+const loginCheck = async (req, res, next) => {
+    let token = null;
+    if (req.headers['authorization']) {
+        token = req.headers['authorization'];
+    }
+
+    if (req.headers['x-xsrf-token']) {
+        token = req.headers['x-xsrf-token'];
+    }
+
+    if (req.query['token']) {
+        token = req.query['token'];
+    }
+
+    if (!token) {
+        next({
+            status: 401,
+            msg: "Unauthenticated"
+        })
+    } else {
+        try {
+            let token_parts = token.split(" ");
+            token = token_parts.pop();
+
+            let data = jwt.verify(token, CONFIG.JWT_SECRET)
+            
+            if (data) {
+                let user = await UserModel.findById(data.id);
+                if (user) {
+                    req.auth_user = user;
+                    next();
+                } else {
+                    next({
+                        status: 401,
+                        msg: "Token expired or user does not exists."
+                    })
+                }
+            } else {
+                next({
+                    status: 401,
+                    msg: "Unauthorized: Token mismatched"
+                })
+            }
+        } catch (err) {
+            next({
+                status: 400,
+                msg: err
+            })
+        }
+    }
+}
+
+module.exports = loginCheck;
+`,
+    authService: `const UserModel = require('../models/user.model');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const CONFIG = require("../../config/config");
+
+class AuthService {
+    loginValidate = (data) => {
+        let msg = null;
+        if (!data.email || !data.password) {
+            msg = "Credentials are required";
+        } else {
+            msg = null;
+        }
+
+        return msg;
+    }
+
+    registerValidate = (data, is_update = false) => {
+        let msg = null;
+
+        if (!data.name) {
+            msg['name'] = "Name is required"
+        }
+
+        if (!data.email) {
+            msg['email'] = "Email is required"
+        }
+
+        if (!data.password) {
+            msg['password'] = "Password is required"
+        }
+
+        if (!data.role) {
+            msg['role'] = "Role is required"
+        }
+
+        if (msg) {
+            throw msg
+
+        } else {
+            return null;
+        }
+        // return msg;
+    }
+
+    register = (data) => {
+        let user = new UserModel(data);
+        return user.save();
+    }
+
+    login = async (data) => {
+        try {
+
+            let user = await UserModel.findOne({
+                email: data.email
+            });
+            if (user) {
+                if (bcrypt.compareSync(data.password, user.password)) {
+                    return user;
+                } else {
+                    throw { status: 400, msg: "Credentials does not match" }
+                }
+            } else {
+                throw { status: 400, msg: "User does not exists." }
+            }
+        } catch (err) {
+            throw err;
+        }
+
+    }
+
+    getToken = (data) => {
+        let token = jwt.sign(data, CONFIG.JWT_SECRET);
+        return token;
+    }
+}
+
+module.exports = AuthService;
+`,
+
+    authRoute: `const router = require('express').Router();
+const AuthController = require("../app/controllers/auth.controller")
+const auth_controller = new AuthController();
+const uploader = require('../app/middleware/uploader.middleware');
+
+router.post('/login', auth_controller.login)
+
+router.post('/register', auth_controller.setPath, uploader.single('image'), auth_controller.register)
+
+module.exports = router;`
+
 }
 
 
